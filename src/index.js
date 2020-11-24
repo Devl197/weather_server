4da161f8b380acae9dd08b2025132a94649b989e
Aleksandar Mladenovic <ml_aleksandar@hotmail.com>
2020-11-24 22:03:18 +0100

    Added Vue app to the server

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const route = require('./routes/weather');
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname + "/build")));
+app.use(express.static(path.join(__dirname + '/build')));
 
 // Routes which should handle requests
 app.use('/api/weather', route);
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/build/index.html"));
-//   });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
